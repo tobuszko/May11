690fc02cd4b0b75c931f45ae5f5c93021442db58
obuszko <tomaszobuszko@gmail.com>
2019-05-11 12:52:53 +0200

    i

--- src/pl/wszib/edu/Generic/GenericTest.java
+++ src/pl/wszib/edu/Generic/GenericTest.java
@@ -9,9 +9,82 @@ public class GenericTest {
 
     public static void main(String[] args) {
 
-        List list = new ArrayList();
-        list.add("test");
+    /*    Box<String> box = new Box();
+        box.setObject("siedem");
+        String s = box.getObject();*/
 
-        String s = (String)list.get(0);
+
+    Pair<Integer, String> p1 = new OrderedPair<>(1, "Apple");
+    Pair<Integer , String> p2 = new OrderedPair<>(2, "pear");
+
+    //Pair<String, Box<Integer>> p3 = new OrderedPair<>("Alina", new Box<Integer>());
+
+boolean same = Util.compare(p1,p2);
+        System.out.println(same);
+    }
+}
+
+// E - element
+// T - type
+// K - key
+// N - number
+// V - value
+// S,U
+
+
+class Box<T> {
+    private T object;
+    public T getObject() {
+        return object;
+    }
+
+    public void setObject(T object) {
+        this.object = object;
+    }
+
+}
+
+
+interface Pair<K, V> {
+    K getKey();
+    V getValue();
+}
+
+class OrderedPair<K, V> implements Pair<K, V>{
+    private K key;
+    private V  value;
+
+    public OrderedPair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    @Override
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+
+
+}
+
+
+
+class Util {
+    public static <K, V> boolean compare (Pair<K, V> p1,  Pair<K, V> p2){
+        return p1.getKey().equals(p2.getKey()) && p1.getValue().equals(p2.getValue());
     }
 }
